ed5069ca38927ad428f60d38d61fd089ea192728
Nassim <nassimelalam@gmail.com>
2019-01-20 09:49:57 +0100

    Commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::resource('posts', 'PostsController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('dash', function () {
+    return view('Back/dashboard');
+    
+});
